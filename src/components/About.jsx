22827f68c4502d7872808090fa9b161554d06780
Shivam Kumar <shivamkumar9969@gmail.com>
2024-06-23 00:55:11 +0530

    social account update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,70 +1,100 @@
-import React from "react";
-
-import aboutBg from "../assets/about-bg.png"
+import React, { useState, useEffect } from "react";
+import aboutBg from "../assets/about-bg.png";
 
 const About = React.forwardRef((props, ref) => {
+  const [activeLink, setActiveLink] = useState(-1);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setActiveLink((prevActiveLink) => (prevActiveLink + 1) % 5);
+    }, 3000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const getActiveClasses = (index) => {
+    // Return additional classes if the link is currently active
+    if (index === activeLink) {
+      switch (index) {
+        case 0:
+          return "text-white border-white bg-black opacity-[0.8]";
+        case 1:
+          return "text-white border-white bg-[#0077b5] opacity-[0.8]";
+        case 2:
+          return "text-white border-white bg-gradient-to-br from-red-500 to-blue-500";
+        case 3:
+          return "text-white border-white bg-[#4267B2] opacity-[.8]";
+        case 4:
+          return "text-white border-2 border-white bg-[#1DA1F2] opacity-[.8]";
+        default:
+          return "";
+      }
+    }
+    return "";
+  };
+
   return (
-    < >
-      <div id="About" style={{ backgroundImage: `url(${aboutBg})` }} className="mt-0 bg-cover bg-no-repeat bg-center h-screen w-screen flex items-center justify-center ">
-        <div className="">
+    <div
+      id="About"
+      style={{ backgroundImage: `url(${aboutBg})` }}
+      className="mt-0 bg-cover bg-no-repeat bg-center h-screen w-screen flex items-center justify-center"
+    >
+      <div className="">
         <div className="details mx-14 bg-transparent text-gray">
-        <h1 className="text-4xl from-neutral-900">Hello,I'm</h1>
+          <h1 className="text-4xl from-neutral-900">Hello, I'm</h1>
           <h1 className="text-6xl from-neutral-900">Shivam Kumar</h1>
           <p className="text-2xl mt-4 font-semibold">A Bit About Me</p>
           <p className="whitespace-pre-line text-base font-medium  from-neutral-900 mt-3">
-            A passionate Fullstack and Software Developer currently pursuing
-            {"\n"} a Master of Computer Applications with a specialization in
-            Cloud {"\n"}Computing and DevOps. With a dynamic skill set and an
-            insatiable{"\n"} curiosity, I'm committed to pushing the boundaries
-            of technology.
+            A passionate Full-Stack and Software Developer completed a Master{"\n"} of Computer Applications with a specialization in Cloud Computing{"\n"} and DevOps. With a dynamic skill set and an insatiable curiosity, I'm{"\n"} committed to pushing the boundaries of technology.
           </p>
-          
-
-          <div className="social-account ">
-            <ul className="flex mt-3 text-3xl" >
-              <li className="m-2">
-                <a className="border-2 border-black block rounded-full relative overflow-hidden  hover:text-white hover:border-white hover:bg-black hover:opacity-[0.8]" href="https://github.com/shivamkumar9969">
-                  <i className="fa-brands fa-github p-1 "></i>
-                </a>
-              </li>
-
-              <li className="m-2">
-                <a className="border-2 border-solid border-black block rounded-full relative overflow-hidden hover:border-white hover:text-white hover:bg-[#0077b5] hover:opacity-[0.8]" href="https://www.linkedin.com/in/shivam-kumar-7b896a254/">
-                  <i className="fa-brands fa-linkedin p-1"></i>
-                </a>
-              </li>
-              <li className="m-2">
-                <a className="border-2 border-solid border-black block rounded-full relative overflow-hidden hover:border-white hover:text-white hover:bg-gradient-to-br hover:from-red-500 hover:to-blue-500 " href="https://www.instagram.com/itz_shiv___/">
-                  <i className="fa-brands fa-instagram p-1"></i>
-                </a>
-              </li >
-
-              <li className="m-2">
-                <a className="border-2 border-solid border-black block rounded-full relative overflow-hidden hover:border-white hover:text-white hover:bg-[#4267B2] hover:opacity-[.8] " href="https://www.facebook.com/profile.php?id=100011586043603">
-                  <i className="fa-brands fa-facebook p-1"></i>
-                </a>
-              </li>
-
-              <li className="m-2">
-                <a className="border-2 border-solid border-black block rounded-full relative overflow-hidden hover:border-white  hover:text-white hover:bg-[#1DA1F2] hover:opacity-[.8]" href="https://twitter.com/shivamkumar9969">
-                  <i className="fa-brands fa-twitter p-1"></i>
-                </a>
-              </li>
+          <div className="social-account">
+            <ul className="flex mt-3 text-4xl">
+              {[
+                {
+                  href: "https://github.com/shivamkumar9969",
+                  icon: "fa-github",
+                },
+                {
+                  href: "https://www.linkedin.com/in/shivam-kumar-7b896a254/",
+                  icon: "fa-linkedin-in",
+                },
+                {
+                  href: "https://www.instagram.com/itz_shiv___/",
+                  icon: "fa-instagram",
+                },
+                {
+                  href: "https://www.facebook.com/profile.php?id=100011586043603",
+                  icon: "fa-facebook",
+                },
+                {
+                  href: "https://twitter.com/shivamkumar9969",
+                  icon: "fa-twitter",
+                },
+              ].map((link, index) => (
+                <li key={index} className="m-2">
+                  <a
+                    className={` border-black block rounded-full relative overflow-hidden p-1
+                      hover:text-white hover:border-white
+                      ${index === 0 ? "hover:bg-black hover:opacity-[0.8]" : ""}
+                      ${index === 1 ? "hover:bg-[#0077b5] hover:opacity-[0.8]" : ""}
+                      ${index === 2 ? "hover:bg-gradient-to-br hover:from-red-500 hover:to-blue-500" : ""}
+                      ${index === 3 ? "hover:bg-[#4267B2] hover:opacity-[.8]" : ""}
+                      ${index === 4 ? "broder-2 hover:bg-[#1DA1F2] hover:opacity-[.8]" : ""}
+                      ${getActiveClasses(index)}`}
+                    href={link.href}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i className={`fa-brands ${link.icon}`}></i>
+                  </a>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
-        {/* <div className="animated-container sm:animate-none">
-          <img
-            className=" animated-image h-96 w-96 bg-cover"
-            src={Pic5}
-            alt="Profile"
-          />
-        </div> */}
-        </div>
       </div>
-
-    </>
+    </div>
   );
-})
+});
 
 export default About;
