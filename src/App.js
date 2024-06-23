e6d31def8d0d89db1563b55780acdc8929c4deb2
Shivam Kumar <shivamkumar9969@gmail.com>
2024-06-23 15:18:17 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	deleted:    src/assets/about-bg.jpg 	deleted:    src/assets/contectbg.jpg 	deleted:    src/assets/contectbg2.jpg 	deleted:    src/assets/contectbg3.jpg 	deleted:    src/assets/contectbg4.jpg 	new file:   src/assets/defiancesystem.jpeg 	new file:   src/assets/drivebuddy.jpeg 	new file:   src/assets/easypeasy.jpeg 	new file:   src/assets/ecommerce.jpeg 	new file:   src/assets/educationmanagement.jpeg 	new file:   src/assets/invoice.png 	deleted:    src/assets/project-bg.jpg 	deleted:    src/assets/skill-bg1.jpg 	modified:   src/components/Project.js

--- src/App.js
+++ src/App.js
@@ -13,9 +13,7 @@ function App() {
       <About/>
       <Skills/>
       <Project/>
-      <Contact/>
-
-   
+      <Contact />
     </div>
   );
 };
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,55 +1,73 @@
-import "./Project.css";
-
-
 import React from 'react';
 import projectBg from "../assets/project-bg2.jpg";
+import eCommerceImg from "../assets/ecommerce.jpeg"; // Replace with actual image paths
+import driveBuddyImg from "../assets/drivebuddy.jpeg"; // Replace with actual image paths
+import defianceSystemImg from "../assets/defiancesystem.jpeg"; // Replace with actual image paths
+import educationManagementImg from "../assets/educationmanagement.jpeg"; // Replace with actual image paths
+import easyPeasyImg from "../assets/easypeasy.jpeg"; // Replace with actual image paths
+import invoice from "../assets/invoice.png"
+const projects = [
+  {
+    title: "EasyPeasy",
+    description: "An intuitive online rental platform for seamless property listing by owners and streamlined search for tenants. Built with React.js, Node.js, Tailwind CSS, Express.js, and MongoDB.",
+    link: "https://github.com/shivamkumar9969/EasyPesy",
+    image: easyPeasyImg
+  },
+  {
+    title: "Invoice Generator",
+    description: "An efficient tool for creating and managing professional invoices. The system simplifies invoice creation, tracks payments, and generates detailed reports. Built with React.js, Node.js, and MongoDB.",
+    link: "https://github.com/shivamkumar9969/invoice-generator/tree/master",
+    image: invoice
+  },
+  {
+    title: "E-Commerce",
+    description: "A comprehensive online shopping platform with React.js, Node.js, Express.js, and MongoDB. Features a user-friendly interface, efficient browsing, and secure payments.",
+    link: "https://github.com/shivamkumar9969/MernStackProject",
+    image: eCommerceImg
+  },
+  {
+    title: "Drive Buddy",
+    description: "A safety system that detects driver drowsiness and notifies emergency contacts during accidents using IoT, Arduino, GPS, GSM, and sensors.",
+    image: driveBuddyImg
+  },
+  {
+    title: "Defiance System",
+    description: "High-security solution for protecting military bases with surveillance, motion sensors, and access controls for real-time monitoring and response.",
+    image: defianceSystemImg
+  },
+  {
+    title: "Education Administration Management",
+    description: "System to simplify school operations, managing teachers, students, and curricula. Handles attendance, grading, scheduling, and online access to grades.",
+    image: educationManagementImg
+  },
+
+];
 
 const Project = React.forwardRef((props, ref) => {
   return (
     <div id="Project" className="bg-cover bg-no-repeat bg-center min-h-screen w-screen" style={{ backgroundImage: `url(${projectBg})` }}>
       <div className="w-screen h-16 bg-[#090909] lg:bg-[#131313]"></div>
-      <div className="text-white text-2xl lg:xl:2xl:text-4xl flex items-center justify-center mt-3">
-        <h1>Project</h1>
+      <div className="text-white text-2xl lg:text-4xl flex items-center justify-center mt-3">
+        <h1>Projects</h1>
       </div>
-
-      <div className="container   ">
-        <div className="min-h-screen flex justify-center items-center">
-
-          <div className="h-screen w-screen flex flex-wrap justify-evenly items-center max-w-screen-xl">
-            <div className="project-box w-full sm:w-90 h-90 flex flex-col justify-center items-center overflow-auto m-1 ">
-              <div className="text-slate-400 text-2xl">E-Commerce</div>
-              <div className="m-3 text-sm text-justify">
-                <p>Our e-commerce project is a comprehensive online shopping platform developed using React.js for the frontend, Node.js and Express.js for the backend, and MongoDB for database management. It offers users a seamless shopping experience with a user-friendly interface, efficient product browsing, and secure payment processing. Leveraging scalable solutions and robust technologies, our project aims to provide customers with an optimal shopping experience while facilitating easy product management for administrators.</p>
-              </div>
-              <a className="text-xs text-blue-700" href="https://github.com/shivamkumar9969/MernStackProject">https://github.com/shivamkumar9969/MernStackProject</a>
-            </div>
-
-            <div className="project-box w-full sm:w-90 h-90 flex flex-col justify-center items-center  overflow-auto m-1">
-              <div className="text-2xl text-slate-400 ">Drive Body</div>
-              <div className="m-3 text-sm text-justify">
-                <p>Drive Buddy is a cutting-edge safety system designed to prevent accidents and safeguard lives. It operates in two key functions: Firstly, it detects signs of driver drowsiness, issuing timely warnings and even activating cruise control for added safety. Secondly, in the event of an accident, it promptly notifies nearby medical facilities, law enforcement, and the driver's emergency contacts, ensuring swift assistance and potentially saving lives. This innovative solution integrates IoT, Arduino, GPS, GSM, and various sensors for its efficient operation.</p>
+      <div className="container mx-auto px-4 py-8">
+        <div className="flex flex-wrap justify-evenly items-center">
+          {projects.map((project, index) => (
+            <div key={index} className="bg-white rounded-lg shadow-lg m-4 p-6 max-w-xs md:max-w-sm transform hover:scale-105 transition-transform duration-300">
+              <img src={project.image} alt={project.title} className="w-full h-40 object-cover rounded-t-lg" />
+              <div className="p-4">
+                <h2 className="text-xl font-bold text-gray-800">{project.title}</h2>
+                <p className="text-gray-600 mt-2">{project.description}</p>
+                {project.link && (
+                  <a href={project.link} className="text-blue-500 mt-4 inline-block" target="_blank" rel="noopener noreferrer">Learn More</a>
+                )}
               </div>
             </div>
-
-            <div className="project-box w-full sm:w-90 h-90 flex flex-col justify-center items-center  overflow-auto m-1">
-              <div className="text-2xl text-slate-400">Difference System</div>
-              <div className="m-3 text-sm text-justify">
-                <p>The "Defiance System" is a high-security solution designed for protecting military bases and other critical installations. It employs advanced technology like surveillance cameras, motion sensors, and access controls to monitor and secure the area. With real-time monitoring and alarm systems, it swiftly detects and responds to unauthorized activities, ensuring the safety of the premises.</p>
-              </div>
-            </div>
-
-            <div className="project-box w-full sm:w-90 h-90 flex flex-col justify-center items-center  overflow-auto m-1">
-              <div className="text-2xl text-slate-500">Education Administration</div>
-              <div className="text-2xl text-slate-500">Management</div>
-              <div className="m-3 text-sm text-justify">
-                <p>The Education Administration Management System simplifies school operations, managing teachers, students, and curricula efficiently. It handles tasks like attendance, grading, and scheduling, while offering online access to grades and coursework for students and parents.</p>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
-  )
-})
+  );
+});
 
 export default Project;
